04edcd4807efd85ac8f55679995cd56c9bc884f2
Afif-Mac <afifmansib123@gmail.com>
2023-11-30 16:20:24 +0700

    pull this

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,29 +4,26 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="bg-orange-400 p-4">
+    <nav className="bg-white p-4" style={{height: "30px"}}>
       <div className="container mx-auto flex justify-between items-center">
-        <div className="flex items-center space-x-4">
+        <div className="flex items-center ">
           {/* Add your logo here */}
           <img
-            src="/your-logo.png"
+            src="/logo.png"
             alt="Logo"
-            className="h-8 w-8 object-contain"
+            className="h-12 w-13 object-contain"
           />
-          <p className="text-white text-lg font-bold">Your App Name</p>
+          <p className="text-orange-500 text-lg font-bold ml-5">G</p>
+          <p className="text-white text-lg font-bold ml-1">POS</p>
         </div>
         {/* Add your navigation links here */}
         <div className="space-x-4">
+        <div className="flex justify-center items-center rounded-2xl p-4" style={{ backgroundColor: "#f47322" }}>
           <a href="#" className="text-white">
-            Home
-          </a>
-          <a href="#" className="text-white">
-            About
-          </a>
-          <a href="#" className="text-white">
-            Contact
+          ติดต่อพนักงานขาย
           </a>
         </div>
+      </div>
       </div>
     </nav>
   );
