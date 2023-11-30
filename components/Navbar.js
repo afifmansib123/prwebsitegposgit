8408f64afbc5600e2ffb4157914acc8c75df1c91
Afif-Mac <afifmansib123@gmail.com>
2023-11-30 16:48:50 +0700

    navbar done

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,29 +1,28 @@
-// components/Navbar.js
-
 import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="bg-white p-4" style={{height: "30px"}}>
+    <nav className="bg-white p-4" style={{ height: "30px" }}>
       <div className="container mx-auto flex justify-between items-center">
-        <div className="flex items-center ">
-          {/* Add your logo here */}
+        <div className="flex items-center">
           <img
             src="/logo.png"
             alt="Logo"
-            className="h-12 w-13 object-contain"
+            className="object-contain"
+            style={{ maxWidth: "100%", height: "auto", width: "100px" }} // Adjust sizes as needed
           />
-          <p className="text-orange-500 text-lg font-bold ml-5">G</p>
-          <p className="text-white text-lg font-bold ml-1">POS</p>
         </div>
         {/* Add your navigation links here */}
         <div className="space-x-4">
-        <div className="flex justify-center items-center rounded-2xl p-4" style={{ backgroundColor: "#f47322" }}>
-          <a href="#" className="text-white">
-          ติดต่อพนักงานขาย
-          </a>
+          <div
+            className="flex justify-center items-center rounded-2xl p-4"
+            style={{ backgroundColor: "#f47322" ,  height: "auto" }}
+          >
+            <a href="#" className="text-white">
+              ติดต่อพนักงานขาย
+            </a>
+          </div>
         </div>
-      </div>
       </div>
     </nav>
   );
--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,7 @@ const Home = () => {
       <div className="container mx-auto p-4">
         {/* Your main content goes here */}
         <div className="flex justify-center items-center rounded-3xl mb-5" style={{height: 80 }}>
-          <h1 className="text-4xl font-bold" style={{ color: "black" }}>
-            ้ำี้รห้กร้หร้รี
-          </h1>
+          
         </div>
         <div className="flex justify-center">
           <Image src="/logo.png" alt="myname" height="800" width="1200"></Image>
