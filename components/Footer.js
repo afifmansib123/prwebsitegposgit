ebd17486628b2d9fde274c0eef6e0f8a0959d3b0
Kanin Sirisuksakulchai <mixkanin@gmail.com>
2023-12-04 17:29:22 +0700

    change footer

---
+++ components/Footer.js
@@ -0,0 +1,53 @@
+import Link from "next/link";
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSquareFacebook,
+  faLine,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+
+const Footer = () => (
+  <div className="grid grid-cols-[42%_2%_55%] bg-black text-white h-40 p-6 content-center justify-stretch lg:px-4">
+    <div className="flex flex-col">
+      <div>
+        <h1 className="text-2xl font-bold">ช่วงเวลาการติดต่อ</h1>
+      </div>
+      <div className="mt-2">
+        <p>ศูนย์บริการลูกค้า: 02-114-8678</p>
+        <p>จันทร์ - ศุกร์: 09:00-18:00 น.</p>
+        <p>เสาร์-อาทิตย์และวันหยุดนักขัตฤกษ์: 10:00-19:00 น.</p>
+      </div>
+    </div>
+    <view className="h-32 w-px bg-white mx-2"></view>
+    <div className="flex flex-col ml-4">
+      <h1 className="text-2xl font-bold">ช่องทางการในติดตามเรา</h1>
+      <div className="grid grid-cols-2">
+        <div className="flex flex-col mt-2">
+          <Link href={"https://www.facebook.com/GposTH"} target="_blank">
+            <FontAwesomeIcon icon={faSquareFacebook} />
+            <span className="ml-2 text-lg">GposTH</span>
+          </Link>
+          <Link href={"https://www.youtube.com/@GposThailand"} target="_blank">
+            <FontAwesomeIcon icon={faYoutube} />
+            <span className="ml-2 text-lg">GposThailand</span>
+          </Link>
+          <Link href={"https://lin.ee/Mh7mvqw"} target="_blank">
+            <FontAwesomeIcon icon={faLine} />
+            <span className="ml-2 text-lg">@Gpos</span>
+          </Link>
+        </div>
+        <div>
+          <Image
+            src="/L_gainfriends_2dbarcodes_BW.png"
+            className="float-right"
+            width='100'
+            height='100'
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Navbar from "../components/Navbar";
 import Image from "next/image";
 
 import ContactForm from "../components/ContactForm";
+import Footer from "@/components/Footer";
 
 const Home = () => {
   return (
@@ -20,41 +21,16 @@ const Home = () => {
             loading="eager"
           ></Image>
         </div>
-        {/* <div className="flex flex-auto justify-center">
-          <Image
-            src="/column-2.png"
-            alt="hello"
-            width={1000}
-            height={1000}
-          ></Image>
-        </div>
-        <div className="flex flex-auto justify-center">
-          <Image
-            src="/column-3.png"
-            alt="hello"
-            width={1000}
-            height={1000}
-          ></Image>
-        </div>
-        <div className="flex flex-auto justify-center">
-          <Image
-            src="/column-4.png"
-            alt="hello"
-            width={1000}
-            height={1000}
-          ></Image>
-        </div> */}
-        <div className="container mx-auto p-4 px-28 mt-8">
+        <div className="container mx-auto p-4 px-16 mt-8">
           {/* Your main content goes here */}
-          <div
-            className="flex justify-center items-center rounded-3xl"
-          >
+          <div className="flex justify-center items-center rounded-3xl">
             {/* Add more content as needed */}
             <ContactForm />
           </div>
         </div>
       </div>
-      <div className="flex flex-auto justify-center mt-6">
+      <Footer />
+      {/* <div className="flex flex-auto justify-center mt-6">
         <Image
           src="/bot03.svg"
           alt="hello"
@@ -62,7 +38,7 @@ const Home = () => {
           height={300}
           loading="eager"
         ></Image>
-      </div>
+      </div> */}
     </div>
   );
 };
