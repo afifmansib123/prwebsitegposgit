2acef1e43e6c4a71eb0743b2969b51febe1379ed
Kanin Sirisuksakulchai <mixkanin@gmail.com>
2023-12-04 15:00:42 +0700

    Update Navbar.js
    adjust navbar size

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,23 +7,23 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-600 p-4">
-      <div className="container mx-auto flex justify-between items-center">
+    <nav className="bg-gray-600 p-6">
+      <div className="container m-auto flex justify-between items-center">
         <div className="flex items-center">
           {/* Add your logo here */}
           <img
             src="/logo.png"
             alt="Logo"
-            className="h-12 w-13 object-contain"
+            className="h-24 w-24 object-contain"
           />
-          <p className="text-orange-500 text-lg font-bold ml-5">G</p>
-          <p className="text-white text-lg font-bold ml-1">POS</p>
+          <p className="text-orange-500 text-2xl font-bold ml-5">G</p>
+          <p className="text-white text-2xl font-bold ml-1">POS</p>
         </div>
         {/* Add your navigation links here */}
         <div className="space-x-4">
           <Link href="#contact-form-section">
             <button
-              className="flex justify-center items-center rounded-2xl p-4 text-white"
+              className="flex justify-center items-center rounded-2xl p-5 text-white text-2xl"
               style={{ backgroundColor: "#f47322" }}
               type="button"
             >
