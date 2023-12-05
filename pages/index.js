d63beb08652b5e83a887fd8ff90c4d3cef1ee110
Kanin Sirisuksakulchai <mixkanin@gmail.com>
2023-12-05 16:35:14 +0700

    replace 3 images

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,20 @@ import Image from "next/image";
 
 import ContactForm from "../components/ContactForm";
 import Footer from "@/components/Footer";
-import salePageImage from "../public/images/salepage_master-01.jpg";
+import columnOne from '../public/images/column1.png';
+import columnTwo from '../public/images/column2.png';
+import columnThree from '../public/images/column3.png';
 
 const Home = () => {
   return (
     <div className={"flex flex-col"}>
       <Navbar />
       <div className="px-4">
-        <div className="flex flex-auto justify-center mt-4">
-          <Image src={salePageImage} alt="" loading="eager" placeholder="blur" priority={true}></Image>
+        <div className="flex flex-col flex-auto justify-center mt-4">
+          {/* <Image src={salePageImage} alt="" loading="eager" placeholder="blur" priority={true}></Image> */}
+          <Image src={columnOne} alt="" loading="eager" placeholder="blur" priority={true}/>
+          <Image src={columnTwo} alt="" loading="eager" placeholder="blur" priority={true}/>
+          <Image src={columnThree} alt="" loading="eager" placeholder="blur" priority={true}/>
         </div>
         <div className="container mx-auto p-4 px-6 mt-8">
           {/* Your main content goes here */}
